d867160ae237e535a4465e25f0be5d69c922f0b9
Ilo Tech <ilo.nikatsadze@gmail.com>
2025-05-07 00:02:44 +0400

    loading state added and hooks were used

--- src/formElements/resetButton/ResetButton.jsx
+++ src/formElements/resetButton/ResetButton.jsx
@@ -1,32 +1,35 @@
+import UseFormReset from "../../controllers/resetController/UseFormReset";
 
-
-const ResetButton = ({setFormData,setRange,setDisplayButton,setShowPassword,displayButton}) => {
-    const handleReset=()=>{
-        setFormData({
-            generatedPassword:"",
-            upperCase:false,
-            lowerCase:false,
-            number:false,
-            char:false
-          });
-          setRange(4);
-          setDisplayButton(false);
-          setShowPassword(false);
-    }
+const ResetButton = ({
+  setFormData,
+  setRange,
+  setDisplayButton,
+  setShowPassword,
+  displayButton,
+  loading,
+}) => {
+  const { handleReset } = UseFormReset({
+    setFormData,
+    setRange,
+    setDisplayButton,
+    setShowPassword,
+  });
   return (
     <>
-    {
-        displayButton?
+      {displayButton ? (
         <div className="flex flex-col justify-center mt-4">
-            <button type="button" onClick={handleReset}
-                className="p-2 cursor-pointer border-2 border-gray-300 rounded-md hover:bg-blue-400 hover:text-white">
-                Reset
-            </button>
-        </div>:null
-    }
-    
+          <button
+            disabled={loading}
+            type="button"
+            onClick={handleReset}
+            className="p-2 cursor-pointer border-2 border-gray-300 rounded-md hover:bg-blue-400 hover:text-white"
+          >
+            Reset
+          </button>
+        </div>
+      ) : null}
     </>
-  )
-}
+  );
+};
 
-export default ResetButton
+export default ResetButton;
